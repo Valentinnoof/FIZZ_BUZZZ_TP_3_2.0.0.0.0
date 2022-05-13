e93ad968c40a20ad1666798c3d47eca2aecd9d57
Your Name <vchassignolle5@gmail.com>
2022-05-13 09:13:43 +0200

    Add fichier MVS_FIZZBUZZ.C MVS_FIZZBUZZ.h

---
+++ MVS_FizzBuzz.h
@@ -0,0 +1,11 @@
+//
+// Created by Valentin Chassignolle on 13/05/2022.
+//
+
+#ifndef GITTP3_MVS_FIZZBUZZ_H
+#define GITTP3_MVS_FIZZBUZZ_H
+
+#define FIZZ "Fizz"
+#define BUZZ "Buzz"
+
+#endif //GITTP3_MVS_FIZZBUZZ_H
